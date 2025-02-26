be054d6469a6e5ce38ee702331b0983456bd7582
曹亦佳 <caoyijia@nyu.edu>
2025-02-26 16:44:29 -0500

    updated prob3.v

--- src/prob3.v
+++ src/prob3.v
@@ -1,9 +1,22 @@
-module tt_um_equal_comparator(
-    input [7:0] A,  // 8-bit input A
-    input [7:0] B,  // 8-bit input B
-    output [7:0] C  // 8-bit output C
+/*
+ * Copyright (c) 2025 Yijia Cao
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+`default_nettype none
+
+module tt_um_example (
+    input  wire [7:0] ui_in,    // Dedicated inputs
+    output wire [7:0] uo_out,   // Dedicated outputs
+    input  wire [7:0] uio_in,   // IOs: Input path
+    output wire [7:0] uio_out,  // IOs: Output path
+    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
+    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       clk,      // clock
+    input  wire       rst_n     // reset_n - low to reset
 );
+
     // Equality comparator logic
-    assign C = (A == B) ? 8'b00000001 : 8'b00000000;  // C[0] = 1 if A == B, else 0
+    assign uo_out = (ui_in == uio_in) ? 8'b00000001 : 8'b00000000;  // C[0] = 1 if A == B, else 0
 
 endmodule
